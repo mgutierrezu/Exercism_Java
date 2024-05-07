06cd5edd32eb008ec915ece6e5e7a491b3779318
Mauricio Gutierrez <mauricio.mgu@gmail.com>
2024-05-07 13:58:04 -0400

    Leap - Exercise Solved

---
+++ java/leap/src/main/java/Leap.java
@@ -0,0 +1,13 @@
+class Leap {
+
+    boolean isLeapYear(int year) {
+        if (year % 100 == 0 && year % 400 == 0) {
+                return true;
+        }
+        else if (year % 100 != 0 && year % 4 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+}
